3dad44442bf88944caa702301539d2b0c88f2619
Hubert Kieruczenko <hub3rt65@gmail.com>
2023-11-08 20:57:09 +0100

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AuthModule,
-    HttpClientModule
+    HttpClientModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { InputComponent } from './input/input.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
-  declarations: [InputComponent],
+  declarations: [InputComponent, HeaderComponent],
   imports: [CommonModule, ReactiveFormsModule],
-  exports: [InputComponent]
+  exports: [InputComponent, HeaderComponent]
 })
 export class SharedModule {}
